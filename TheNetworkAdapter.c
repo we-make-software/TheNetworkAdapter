c0d87f526e7b4347b1fee992e3c21b2245186327
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-23 19:39:47 +0000

    Update on 2025-03-23 19:39:47

--- TheNetworkAdapter.c
+++ TheNetworkAdapter.c
@@ -1,8 +1,8 @@
 #include "../TheNetworkAdapter/TheNetworkAdapter.h"
-SetupTM(TheNetworkAdapter,0,0,  255,255,15,14,13,
-                                12,11,10,9,8,
-                                7,6,5,4,3,
-                                2,1)
+SetupTM(TheNetworkAdapter,0,0,  255,255,255,255,255,
+                                255,255,255,255,255,
+                                255,255,255,255,255,
+                                255,255)
 static LIST_HEAD(NetworkAdapterList);
 static struct NetworkAdapter*Get(u8*value){
     struct TheMailConditioner*tmcTM=GetTheMailConditioner(value,6,false);
@@ -13,13 +13,7 @@ static struct NetworkAdapter*Add(u8*value){
     struct TheMailConditioner*tmcTM=GetTheMailConditioner(value,6,true);
     if(!tmcTM)return NULL;
     if(!GetTheMailConditionerData(tmcTM)){
-        struct NetworkAdapter*objTM=(*GetTheMailConditionerUnsafeData(tmcTM)=kmalloc(sizeof(struct NetworkAdapter),GFP_KERNEL));
-        if(objTM){
 
-        }else{
-            printk(KERN_ERR "TheNetworkAdapter: Can't allocate memory for the NetworkAdapter.\n");
-            return NULL;
-        }
 
     }
     return NULL;
@@ -30,5 +24,5 @@ static void TMStart(void){
     printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been started.\n");
 }
 static void TMEnd(void*){
- 
+    printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been ended.\n");
 }
