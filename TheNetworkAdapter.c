92e66a35ab6bfb70dcc3dbf759af2cf7000c47ce
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 19:55:13 +0000

    Update on 2025-03-24 19:55:13

--- TheNetworkAdapter.c
+++ TheNetworkAdapter.c
@@ -1,8 +1,15 @@
 #include "TheNetworkAdapter.h"
 #include <linux/netdevice.h>
 SetupTM(TheNetworkAdapter,0,0,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
+static bool GetAdd(void);
+static bool GetAdd(void){
+    printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter GetAdd has been called.\n");
+    return true;
+}
+
 static void TMStart(void){
     GetTheNetworkAdapterTM();
+    SetTM(TheNetworkAdapterTM,GetAdd);
     printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been started.\n");
 }
 static void TMEnd(void*){
--- TheNetworkAdapter.h
+++ TheNetworkAdapter.h
@@ -1,12 +1,10 @@
 #ifndef TheNetworkAdapter_H
 #define TheNetworkAdapter_H
 #include "../TheMaintainer/TheMaintainer.h"
-struct NetworkAdapter{
-    SetupEWB;
-    struct net_device*dev;
-};
+
 struct TheNetworkAdapter{
     SetupEWB;
+    bool(*GetAdd)(void);
 };
 #define GetTheNetworkAdapterTM()TM(TheNetworkAdapter,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
 #define GetTheNetworkAdapterTMFailback(failback)TMFailback(TheNetworkAdapter,failback,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
