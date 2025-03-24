a4bac2ba504be0a5b8e3a21a7e1794d2e90c4378
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 21:36:27 +0000

    Update on 2025-03-24 21:36:27

--- TheNetworkAdapter.c
+++ TheNetworkAdapter.c
@@ -1,15 +1,24 @@
 #include "TheNetworkAdapter.h"
 #include <linux/netdevice.h>
+#include <linux/rculist.h>
+#include <linux/etherdevice.h> 
 SetupTM(TheNetworkAdapter,0,0,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
-static bool GetAdd(void);
-static bool GetAdd(void){
-    printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter GetAdd has been called.\n");
-    return true;
+static struct net_device*Get(u8*value);
+static struct net_device*Get(u8*value){
+    struct net_device*dev=NULL;
+    rcu_read_lock();
+    for_each_netdev_rcu(&init_net,dev) {
+        if (memcmp(dev->dev_addr,value,ETH_ALEN)==0) {
+            rcu_read_unlock();
+            return dev;
+        }
+    }
+    rcu_read_unlock();
+    return NULL;
 }
-
 static void TMStart(void){
     GetTheNetworkAdapterTM();
-    SetTM(TheNetworkAdapterTM,GetAdd);
+    SetTM(TheNetworkAdapterTM,Get);
     printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been started.\n");
 }
 static void TMEnd(void*){
--- TheNetworkAdapter.h
+++ TheNetworkAdapter.h
@@ -1,10 +1,9 @@
 #ifndef TheNetworkAdapter_H
 #define TheNetworkAdapter_H
 #include "../TheMaintainer/TheMaintainer.h"
-
 struct TheNetworkAdapter{
     SetupEWB;
-    bool(*GetAdd)(void);
+    struct net_device*(*Get)(u8*value);
 };
 #define GetTheNetworkAdapterTM()TM(TheNetworkAdapter,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
 #define GetTheNetworkAdapterTMFailback(failback)TMFailback(TheNetworkAdapter,failback,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
