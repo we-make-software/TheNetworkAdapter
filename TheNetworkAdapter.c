00ce0c3dd0dfb2c62e700d2ddef1523713d8e0c0
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 09:46:01 +0000

    Update on 2025-03-24 09:46:01

--- TheNetworkAdapter.c
+++ TheNetworkAdapter.c
@@ -1,22 +1,12 @@
 #include "../TheNetworkAdapter/TheNetworkAdapter.h"
 #include <linux/netdevice.h>
 SetupTM(TheNetworkAdapter,0,0,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
-SetupGetAddTMInCode(TheNetworkAdapter,NetworkAdapter,6,{
-    struct net_device* dev;
-    for_each_netdev(&init_net, dev) {
-        if (memcmp(dev->dev_addr,value,6)==0) {
-            data->dev = dev;
-            break;
-        }
-    }
-})
 static void TMStart(void){
     GetTheNetworkAdapterTM();
-    SetupGetAddTMInMainTMStart(TheNetworkAdapterTM);
     printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been started.\n");
 }
 static void TMEnd(void*){
     GetTheNetworkAdapterTM();
-    SetupGetAddTMInMainTMEnd(TheNetworkAdapterTM);
+ 
     printk(KERN_INFO "TheNetworkAdapter: The NetworkAdapter TM has been ended.\n");
 }
--- TheNetworkAdapter.h
+++ TheNetworkAdapter.h
@@ -3,13 +3,10 @@
 #include "../TheMaintainer/TheMaintainer.h"
 struct NetworkAdapter{
     SetupEWB;
-    SetupGetAddTMInData();
     struct net_device*dev;
 };
 struct TheNetworkAdapter{
     SetupEWB;
-    SetupGetAddTMInStruct(NetworkAdapter);
 };
 #define GetTheNetworkAdapterTM()TM(TheNetworkAdapter,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
-
 #endif
