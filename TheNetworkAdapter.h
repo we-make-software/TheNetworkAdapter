9cb71b6fc49de4834a79ca4250df7dfa9e1d0a70
Pirasath Luxchumykanthan <hello@how-do-i-get-your-attention.com>
2025-03-24 22:38:48 +0000

    Update on 2025-03-24 22:38:48

--- TheNetworkAdapter.h
+++ TheNetworkAdapter.h
@@ -1,10 +1,17 @@
 #ifndef TheNetworkAdapter_H
 #define TheNetworkAdapter_H
 #include "../TheMaintainer/TheMaintainer.h"
+struct ReviewPacketData{
+    u8 reverse[(3*sizeof(void*))+2]; 
+    u8*data;
+    u16 SourcePort;
+    bool IsTransmissionControlProtocol;
+    bool IsVersion6;
+};
 struct TheNetworkAdapter{
     SetupEWB;
     struct net_device*(*Get)(u8*value);
 };
 #define GetTheNetworkAdapterTM()TM(TheNetworkAdapter,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
-#define GetTheNetworkAdapterTMFailback(failback)TMFailback(TheNetworkAdapter,failback,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
+//#define GetTheNetworkAdapterTMFailback(failback)TMFailback(TheNetworkAdapter,failback,254,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255)
 #endif
